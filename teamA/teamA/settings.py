6c02937d84fa66318feaeffd5364527f3bf9881f
ToshikiTaninaka <160687252+ToshikiTaninaka@users.noreply.github.com>
2024-06-24 17:14:59 +0900

    aaa

--- teamA/teamA/settings.py
+++ teamA/teamA/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PoftfolioConfig',
+    'requestForm.apps.RequestFormConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
